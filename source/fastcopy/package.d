21d7aa53fae4c6ad28779c69bca1187bbb92ce54
Hiroki Noda <kubo39@gmail.com>
2018-12-16 06:32:25 +0900

    FIX: set syscall nr copy_file_range

--- source/fastcopy/package.d
+++ source/fastcopy/package.d
@@ -39,7 +39,9 @@ version(linux)
 
     private extern (C) int syscall(size_t ident, size_t n, size_t arg1, size_t arg2, size_t arg3, size_t arg4, size_t arg5) @nogc nothrow;
 
-    immutable size_t __NR_COPY_FILE_RANGE;
+    version (X86_64)
+        immutable size_t __NR_COPY_FILE_RANGE = 326;
+    else static assert (false);
 }
 
 
